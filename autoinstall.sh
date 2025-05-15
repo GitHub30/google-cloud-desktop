6a61004c54016f7827883034657ad4eb8ab64a1d
GitHub30 <funaox@gmail.com>
2025-05-15 15:32:28 +0000

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,8 +1,8 @@
-while fuser /var/lib/apt/lists/lock >/dev/null 2>&1; do sleep 1; done
+while fuser /var/lib/apt/lists/lock; do echo waiting; sleep 1; done
 curl -s https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/google.gpg
 echo deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main >>/etc/apt/sources.list.d/chrome-remote-desktop.list
 echo deb [arch=amd64] https://dl.google.com/linux/chrome/deb stable main >>/etc/apt/sources.list.d/chrome.list
 apt-get -qq update
-DEBIAN_FRONTEND=noninteractive apt-get -qq install desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop google-chrome-stable
+DEBIAN_FRONTEND=noninteractive apt-get -qq -o DPkg::Lock::Timeout=-1 install desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop google-chrome-stable
 systemctl disable colord lightdm
 echo exec /etc/X11/Xsession /usr/bin/xfce4-session >/etc/chrome-remote-desktop-session
