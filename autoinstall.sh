3c10be68e41200f64b9f3d5856777f69c820a2bb
GitHub30 <funaox@gmail.com>
2025-05-15 15:19:16 +0000

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,4 +1,3 @@
-sleep 20
 curl -s https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/google.gpg
 echo deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main >>/etc/apt/sources.list.d/chrome-remote-desktop.list
 echo deb [arch=amd64] https://dl.google.com/linux/chrome/deb stable main >>/etc/apt/sources.list.d/chrome.list
