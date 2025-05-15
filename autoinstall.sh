a880b9b747c758918ae362e894bde18e31ede9c9
GitHub30 <funaox@gmail.com>
2025-05-15 12:57:40 +0000

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,11 +1,12 @@
+curl https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /etc/apt/trusted.gpg.d/google.gpg
 echo deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main | tee -a /etc/apt/sources.list.d/chrome-remote-desktop.list
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop
+DEBIAN_FRONTEND=noninteractive apt install --assume-yes desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop
 systemctl disable lightdm.service
 echo exec xfce4-session > /etc/chrome-remote-desktop-session
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install --assume-yes --fix-broken google-chrome-stable_current_amd64.deb
+apt install --assume-yes --fix-broken google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
 echo startup-script completed
