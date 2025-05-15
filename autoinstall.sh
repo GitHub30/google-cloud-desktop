7b8d3c8ad24c3eb1a23b900c5d9b54a6b2a7031d
GitHub30 <funaox@gmail.com>
2025-05-15 14:10:14 +0000

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,6 +2,6 @@ curl -s https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor -o /et
 echo deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main >>/etc/apt/sources.list.d/chrome-remote-desktop.list
 echo deb [arch=amd64] https://dl.google.com/linux/chrome/deb stable main >>/etc/apt/sources.list.d/chrome.list
 apt-get -qq update
-DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Lock::Timeout=60 install -qq desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop google-chrome-stable
+DEBIAN_FRONTEND=noninteractive apt-get -qq -o DPkg::Lock::Timeout=60 install desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop google-chrome-stable
 systemctl disable lightdm # https://cloud.google.com/architecture/chrome-desktop-remote-on-compute-engine#:~:text=disable%20the%20display%20manager
 echo exec /etc/X11/Xsession /usr/bin/xfce4-session >/etc/chrome-remote-desktop-session # https://cloud.google.com/architecture/chrome-desktop-remote-on-compute-engine#choose_a_different_desktop_environment
