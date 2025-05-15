9c9af1dc47f32eb4e73290787f3f48d6993bf1d7
GitHub30 <funaox@gmail.com>
2025-05-15 14:35:14 +0000

    update

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,8 +3,5 @@ echo deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stab
 echo deb [arch=amd64] https://dl.google.com/linux/chrome/deb stable main >>/etc/apt/sources.list.d/chrome.list
 apt-get -qq -o DPkg::Lock::Timeout=60 update
 DEBIAN_FRONTEND=noninteractive apt-get -qq -o DPkg::Lock::Timeout=60 install desktop-base xscreensaver dbus-x11 xfce4 task-xfce-desktop less bzip2 zip unzip tasksel wget chrome-remote-desktop google-chrome-stable
-# https://cloud.google.com/architecture/chrome-desktop-remote-on-compute-engine#:~:text=disable%20the%20display%20manager
-# https://zenn.dev/google_cloud_jp/articles/chrome-remote-desktop#:~:text=colord
 systemctl disable lightdm colord
-# https://cloud.google.com/architecture/chrome-desktop-remote-on-compute-engine#choose_a_different_desktop_environment
 echo exec /etc/X11/Xsession /usr/bin/xfce4-session >/etc/chrome-remote-desktop-session
